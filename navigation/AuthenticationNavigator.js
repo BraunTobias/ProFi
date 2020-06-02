7f8e83b8dcf28e41c1177a433b1e9bf2188fa113
BraunTobias <braun_tobias@gmx.net>
2020-06-02 20:59:41 +0200

    dummy data login -merged

--- navigation/AuthenticationNavigator.js
+++ navigation/AuthenticationNavigator.js
@@ -23,18 +23,4 @@ export default AuthenticationNavigator = () => {
             </AuthenticationStack.Navigator>
         </NavigationContainer>
     );
-};
-=======
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import LoginScreen from '../screens/LoginScreen';
-import RegistrationScreen from '../screens/RegistrationScreen';
-import MainNavigator from '../navigation/MainNavigator';
-
-const AuthenticationNavigator = createSwitchNavigator({
-    Login: LoginScreen,
-    Registration: RegistrationScreen,
-    Main: MainNavigator,
-});
-
-export default createAppContainer(AuthenticationNavigator);
 >>>>>>> a43f41048d95d9e92f3effc95e0f4f8c67132882
