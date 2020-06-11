3f0e471ec709382ab1c2495ef2c52a56d644858c
nkolbeck <nicolas.kolbeck@gmail.com>
2020-06-11 14:37:57 +0200

    test

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+//Test
 import React, { useState } from "react";
 import AuthenticationNavigator from './navigation/AuthenticationNavigator';
 import MainNavigator from './navigation/MainNavigator';
